0b509445b7885da3cfe78253d9e4c1ebf2011f92
poter0501 <50819216+poter0501@users.noreply.github.com>
2023-03-21 11:13:28 +0900

    [Gold III] Title: 아침 산책, Time: 3552 ms, Memory: 115404 KB -BaekjoonHub

---
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -0,0 +1,55 @@
+import sys
+
+n = int(sys.stdin.readline())
+in_out = [int(c) for c in sys.stdin.readline().rstrip()]
+edges = {}
+for _ in range(n - 1):
+    x, y = map(int, sys.stdin.readline().split())
+    try:
+        edges[x - 1].add(y - 1)
+    except KeyError:
+        edges[x - 1] = set()
+        edges[x - 1].add(y - 1)
+    try:
+        edges[y - 1].add(x - 1)
+    except KeyError:
+        edges[y - 1] = set()
+        edges[y - 1].add(x - 1)
+
+visited = [0 for i in range(n)]
+path = []
+
+
+def dfs(curr, count=0):
+    if visited[curr] != 0:
+        return
+    else:
+        if in_out[curr] == 1 and count != 0:
+            path.append(curr)
+            return
+        else:
+            visited[curr] = 1
+            path.append(curr)
+            can_go = edges.get(curr, False)
+            if can_go:
+                for go in can_go:
+                    dfs(go, count + 1)
+
+
+# print(f' 실내외: {in_out}')
+total_count = 0
+for i, in_or_out in enumerate(in_out):
+    if in_or_out == 1:
+        dfs(i)
+        # print(f'start={i}, path = {path}')
+        start_idx = 0
+        end_idx = start_idx + 1
+        possible = []
+        for p in path[1:]:
+            if in_out[p] == 1:
+                possible.append(p)
+                total_count += 1
+        # print(f'start={i}, possible={possible}')
+        path = list()
+        visited = [0 for i in range(n)]
+print(total_count)

a465602b546a38f23e5da599f23586269e8a0e1d
poter0501 <50819216+poter0501@users.noreply.github.com>
2023-03-22 01:45:57 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 60 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,42 +1,31 @@
-import copy
 import sys
-from itertools import permutations
 
 n = int(sys.stdin.readline())
-a = list(map(int, sys.stdin.readline().split()))
-operator_dict = {0: '+', 1: '-', 2: '*', 3: '/'}
-operators = []
-for i, operator_count in enumerate(list(map(int, sys.stdin.readline().split()))):
-    for j in range(operator_count):
-        operators.append(operator_dict[i])
-# print(operators)
-val_min = float('inf')
-val_max = float('-inf')
-for way in map(list, permutations(operators, len(operators))):
-    temp_a = copy.deepcopy(a)
-    left_side = temp_a.pop(0)
-    right_side = temp_a.pop(0)
-    while True:
-        oprt = way.pop(0)
-        if oprt == '+':
-            left_side = left_side + right_side
-        elif oprt == '-':
-            left_side = left_side - right_side
-        elif oprt == '*':
-            left_side = left_side * right_side
-        else:
-            if left_side < 0:  # right_side 의 경우 a 리스트 안에 있던 원소라 항상 양수
-                left_side = abs(left_side) // abs(right_side)
-                left_side = -left_side
-            else:
-                left_side = left_side // right_side
-        if len(temp_a) > 0:
-            right_side = temp_a.pop(0)
-        else:
-            break
-    if left_side < val_min:
-        val_min = left_side
-    if left_side > val_max:
-        val_max = left_side
-print(val_max)
-print(val_min)
+numbers = list(map(int, sys.stdin.readline().split()))
+add_count, sub_count, mul_count, div_count = map(int, sys.stdin.readline().split())
+max_val = float('-inf')
+min_val = float('inf')
+
+
+def dfs(add, sub, mul, div, sum, idx):
+    global max_val, min_val
+    
+    if idx == n:
+        max_val = max(max_val, sum)
+        min_val = min(min_val, sum)
+        return
+    else:
+        if add > 0:
+            dfs(add - 1, sub, mul, div, sum + numbers[idx], idx + 1)
+        if sub > 0:
+            dfs(add, sub - 1, mul, div, sum - numbers[idx], idx + 1)
+        if mul > 0:
+            dfs(add, sub, mul - 1, div, sum * numbers[idx], idx + 1)
+        if div > 0:
+            dfs(add, sub, mul, div - 1,
+                sum // numbers[idx] if sum > 0 and numbers[idx] > 0 else -(abs(sum) // abs(numbers[idx])), idx + 1)
+
+
+dfs(add_count, sub_count, mul_count, div_count, numbers[0], 1)
+print(max_val)
+print(min_val)

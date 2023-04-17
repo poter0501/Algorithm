f83e09ea47c837664263c2accf5820d6514a709a
poter0501 <50819216+poter0501@users.noreply.github.com>
2023-04-17 10:39:51 +0900

    [Silver II] Title: A → B, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,29 +1,25 @@
 import sys
 
-a, b = map(int, sys.stdin.readline().split())
+a, b = sys.stdin.readline().rstrip().split()
 
+b_last = b[-1]
 count = 1
-a_str = str(a)
-b_str = str(b)
-
-while True:
-    last_digit = b_str[-1]
-    if last_digit != '1':
-        if int(last_digit) % 2 != 0:
+while len(a) <= len(b):
+    
+    if b[-1] == '1':
+        b = b[:len(b) - 1]
+        count += 1
+    else:
+        if int(b[-1]) % 2 != 0:
             print(-1)
             exit()
-    
-    if last_digit == '1':
-        count += 1
-        b_str = b_str[:-1]
-    else:
-        count += 1
-        b_str = str(int(b_str) // 2)
-    
-    if b_str == a_str:
+        else:
+            b = str(int(b) // 2)
+            count += 1
+    if a == b:
         break
-    if int(b_str) < a:
-        count = -1
-        break
-        
+
+if a != b:
+    print(-1)
+    exit()
 print(count)

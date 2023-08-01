e277c71bac8306efaa79ec14ef7e34d408cb51f8
poter0501 <50819216+poter0501@users.noreply.github.com>
2023-08-01 16:24:25 +0900

    [Bronze I] Title: 소인수분해, Time: 1492 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -5,20 +5,11 @@ n = int(n)
 if n == 1:
     print()
     exit()
-is_prime = [True for i in range(n + 1)]
-is_prime[0] = False
-is_prime[1] = False
 
-for i in range(2, n):
-    if is_prime[i]:
-        for j in range(1, n // i + 1):
-            if j != 1:
-                is_prime[i * j] = False
-
-while not is_prime[n]:
-    for i in range(2, n + 1):
-        if n % i == 0:
-            print(i)
-            n = n // i
-            break
-print(n)
+i = 2
+while n != 1:
+    if n % i == 0:
+        print(i)
+        n = n // i
+    else:
+        i += 1

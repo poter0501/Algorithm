d206ce288142213f7d1eff580d84ad9d472db6d0
poter0501 <50819216+poter0501@users.noreply.github.com>
2023-08-02 16:49:05 +0900

    [Bronze II] Title: 블랙잭, Time: 304 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,26 +1,14 @@
 import sys
-
-
-def combination(combi, num):
-    result = []
-    if num == 1:
-        result = [[i] for i in combi]
-        return result
-    else:
-        for i, c in enumerate(combi):
-            if i == 0:
-                result = result + [[combi[i]] + j for j in combination(combi[i + 1:], num - 1)]
-            elif i == len(combi) - 1:
-                result = result + [[combi[i]] + j for j in combination(combi[:i], num - 1)]
-            else:
-                result = result + [[combi[i]] + j for j in combination(combi[:i] + combi[i + 1:], num - 1)]
-    return result
-
+from itertools import permutations
 
 n, m = map(int, sys.stdin.readline().split())
-
 cards = list(map(int, sys.stdin.readline().split()))
-combinations = combination(cards, 3)
-sum_list = [sum(i) for i in combinations]
-sum_list = list(filter(lambda x: x <= m, sum_list))
-print(max(sum_list))
+
+answer = float('-inf')
+for pmt in permutations(cards, 3):
+    sum_val = sum(pmt)
+    if answer < sum_val <= m:
+        answer = sum_val
+        if sum_val == m:
+            break
+print(answer)

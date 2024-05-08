02707883b8e02a308dd71665133f1d78aea3634f
dylee <dy.lee@sparta-devcamp.com>
2024-05-08 16:08:55 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+def solution(s):
+    answer = 0
+    # is_correct_parenthesis(s)
+    for i in range(0, len(s)):
+        sub=s[i: ]+s[0:i]
+        if is_correct_parenthesis(sub):
+            answer+=1
+    return answer
+
+def is_correct_parenthesis(s):
+    s = deque(s)
+    stack = deque()
+    while s:
+        curr = s.popleft()
+        if not stack:
+            stack.append(curr)
+            continue
+        
+        if stack[-1]+curr=='()' or stack[-1]+curr=='{}' or stack[-1]+curr=='[]':
+            stack.pop()
+            continue
+        
+        stack.append(curr)
+            
+    return len(s)==0 and len(stack)==0

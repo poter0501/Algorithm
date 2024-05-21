b6aada30dec46301d67d538036916a35528ae54d
dylee <dy.lee@sparta-devcamp.com>
2024-05-21 10:17:19 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -2,6 +2,6 @@ def solution(n):
     answer = 0
     for i in range(1, n+1):
         answer+=1
-        while answer==3 or answer%3==0 or str(answer).count('3')>0:
+        while answer%3==0 or str(answer).count('3')>0:
             answer+=1
     return answer
